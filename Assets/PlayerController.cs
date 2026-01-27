3f61174a8f270314461db6d7b27526da5b23d21c
Joe-Dob <25897445@stu.mmu.ac.uk>
2026-01-27 02:18:17 +0000

    Redoing the player controller script

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,12 +4,14 @@ public class NewMonoBehaviourScript : MonoBehaviour{
 
     Rigidbody2D rigidBody;
     public float speed;
-
+    public float jumpForce = 8.0f;
+    public float airControlForce = 10.0f;
+    public float airControlMax = 1.5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
