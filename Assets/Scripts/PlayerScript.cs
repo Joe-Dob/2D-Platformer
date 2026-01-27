7b51087be46a10b5745861c80b7f26a0ffb49803
Joe-Dob <25897445@stu.mmu.ac.uk>
2026-01-27 07:29:37 +0000

    player script broke and had to fix it

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,20 +1,19 @@
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using UnityEngine;
 
-
-public class NewMonoBehaviourScript : MonoBehaviour{
-
-public class PlayerScript : MonoBehaviour {
-
+public class PlayerScript : MonoBehaviour
+{
     Rigidbody2D rigidBody;
     Animator animator;
+
     public float speed = 5.0f;
     public float jumpForce = 8.0f;
     public float airControlForce = 10.0f;
     public float airControlMax = 1.5f;
+
     public bool grounded;
     public AudioSource coinSound;
- 
+
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
@@ -23,30 +22,36 @@ public class PlayerScript : MonoBehaviour {
 
     void Update()
     {
-        if(rigidBody.linearVelocity.x*transform.localScale.x < 0.0f)
+        if (rigidBody.linearVelocity.x * transform.localScale.x < 0.0f)
             transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+
         float xSpeed = Mathf.Abs(rigidBody.linearVelocity.x);
         animator.SetFloat("xspeed", xSpeed);
+
         float ySpeed = rigidBody.linearVelocity.y;
         animator.SetFloat("yspeed", ySpeed);
+
         float blinkVal = Random.Range(0.0f, 1000.0f);
         if (blinkVal < 1.0f)
             animator.SetTrigger("blinktrigger");
     }
 
     private void FixedUpdate()
-        {
-            float h = Input.GetAxis("Horizontal");
-            // check if we're on the ground
-            if (!grounded)
-            {
-                return;
-            }
+    {
+        float h = Input.GetAxis("Horizontal");
 
-            if (Input.GetAxis("Jump") > 0.0f)
-                rigidBody.AddForce(new Vector2(0.0f, jumpForce), ForceMode2D.Impulse);
-            else
-                rigidBody.linearVelocity = new Vector2(speed * h, rigidBody.linearVelocity.y);
+        // check if we're on the ground
+        if (!grounded)
+        {
+            return;
+        }
+
+        if (Input.GetAxis("Jump") > 0.0f)
+            rigidBody.AddForce(new Vector2(0.0f, jumpForce), ForceMode2D.Impulse);
+        else
+            rigidBody.linearVelocity = new Vector2(speed * h, rigidBody.linearVelocity.y);
+    }   // ✅ THIS was the missing brace
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.layer == 3)
@@ -54,6 +59,7 @@ public class PlayerScript : MonoBehaviour {
             grounded = true;
         }
     }
+
     private void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.layer == 3)
@@ -64,10 +70,11 @@ public class PlayerScript : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if ( collision.gameObject.tag == "Coin")
+        if (collision.gameObject.tag == "Coin")
         {
             Destroy(collision.gameObject);
             coinSound.Play();
         }
     }
 }
+
